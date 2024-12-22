436a7e63e0e43b8bd5bdade0cc570da143a966c9
Lostinaa <Lostinaa@github.com>
2024-12-22 13:47:03 +0300

    initial commit

--- docing.js
+++ docing.js
@@ -1,10 +1,10 @@
   // Select the canvas element
-function fillcircle(ctx,x,y, radius)
+function fillcircle(ctx,x,y, radius, color = "green"){
   const canvas = document.getElementById("game");
   const width = canvas.width;
   const height = canvas.height;
   const ctx = canvas.getContext("2d"); // Get the 2D drawing context
-
+}
   // Draw a rectangle
   ctx.fillStyle = "blue";
   ctx.fillRect(50, 50, 150, 100);
@@ -14,7 +14,6 @@ function fillcircle(ctx,x,y, radius)
   ctx.arc(300, 150, 50, 0, Math.PI * 2, false);
   ctx.fillStyle = "red";
   ctx.fill();
-
   // Draw text
   ctx.font = "20px Arial";
   ctx.fillStyle = "green";
