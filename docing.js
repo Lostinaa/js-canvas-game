9a7570c80783105abc9a39e2f9696931daf32716
lostinaa <docbeatz8@gmail.com>
2025-01-04 20:55:46 +0300

    update

--- docing.js
+++ docing.js
@@ -1,37 +1,46 @@
   // Select the canvas element
 function fillcircle(ctx, x,y, radius, color = "black"){
-  ctx.beginPath();
-  ctx.arc(x, y, 50, 0, Math.PI * 2, false);
-  ctx.fillStyle = "#866BF0";
-  ctx.fill();
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, Math.PI * 2, false);
+    ctx.fillStyle = "#866BF0";
+    ctx.fill();
 
 }
-let start;
+      const canvas = document.getElementById("game");
+      const width = canvas.width;
+      const height = canvas.height;
+      const ctx = canvas.getContext("2d"); // Get the 2D drawing context
 
-function step(timestamp){
- if (start === undefined) {
-  start = timestamp;
+  let start;
+  let x = width /2
+  let y = height /2
+  let dx = 100
+  let dy = 100  
+
+    function step(timestamp){
+        if (start === undefined) {
+            start = timestamp;
  }
-  const elapsed = timestamp - start;
-  start = timestamp;
-  
-  console.log(elapsed);
-  window.requestAnimationFrame(step);
+    const dt = (timestamp - start)/1000;
+      start = timestamp;
+  x += dx * dt;
+  y += dy * dt;
+
+  fillcircle(ctx, x,  y, 70, "black")      
+    window.requestAnimationFrame(step);
 }
-window.requestAnimationFrame(step);
-  const canvas = document.getElementById("game");
-  const width = canvas.width;
-  const height = canvas.height;
-  const ctx = canvas.getContext("2d"); // Get the 2D drawing context
-
-  // Draw a rectangle
-  ctx.fillStyle = "#54B5CD";
-  ctx.fillRect(50, 50, 150, 100);
-
-  // Draw a circle
-  fillcircle(ctx, width / 2, height / 2, 50, "black")
+          window.requestAnimationFrame(step);
   
-  // Draw text
-  ctx.font = "20px Arial";
-  ctx.fillStyle = "indigo";
-  ctx.fillText("Hello Canvas!", 200, 300);
+
+    // Draw a rectangle
+      ctx.fillStyle = "#54B5CD";
+      ctx.fillRect(50, 50, 150, 100);
+
+    // Draw a circle
+      fillcircle(ctx, x,  y, 70, "black")
+    
+    
+    // Draw text
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "indigo";
+      ctx.fillText("Hello Canvas!", 200, 300);
