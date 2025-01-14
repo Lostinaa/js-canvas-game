8b5e343d43e73b38d86bbbaba43666d25bb6ebbe
lostinaa <docbeatz8@gmail.com>
2025-01-14 19:31:39 +0300

    update

--- docing.js
+++ docing.js
@@ -9,9 +9,10 @@ function fillcircle(ctx, x,y, radius, color = "black"){
       const canvas = document.getElementById("game");
       const width = canvas.width;
       const height = canvas.height;
-      const ctx = canvas.getContext("2d"); // Get the 2D drawing context
+      const ctx = canvas.getContext("2d"); // Get the  2D drawing context
 
   let start;
+  let radius = 100;
   let x = width /2
   let y = height /2
   let dx = 100
@@ -23,10 +24,18 @@ function fillcircle(ctx, x,y, radius, color = "black"){
  }
     const dt = (timestamp - start)/1000;
       start = timestamp;
+
+  if (x + radius >= width || x - radius <= 0) dx = -dx;
+  if (y + radius >= height || y - radius <= 0) dy = -dy;
+  
   x += dx * dt;
   y += dy * dt;
 
-  fillcircle(ctx, x,  y, 70, "black")      
+
+
+  ctx.clearRect(0, 0, width, height);
+
+  fillcircle(ctx, x,  y, 70, "btellack")      
     window.requestAnimationFrame(step);
 }
           window.requestAnimationFrame(step);
@@ -40,7 +49,3 @@ function fillcircle(ctx, x,y, radius, color = "black"){
       fillcircle(ctx, x,  y, 70, "black")
     
     
-    // Draw text
-      ctx.font = "20px Arial";
-      ctx.fillStyle = "indigo";
-      ctx.fillText("Hello Canvas!", 200, 300);
